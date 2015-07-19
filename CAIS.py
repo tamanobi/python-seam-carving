a482f4772327db3943ef057d694d90e49a559aad
Kohki YAMAGIWA <tamanobi@gmail.com>
2015-07-19 16:26:25 +0900

    modified import

--- CAIS.py
+++ CAIS.py
@@ -46,7 +46,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-import Image
+from PIL import Image
 from scipy.ndimage.filters import generic_gradient_magnitude, sobel
 import numpy
 from math import fabs
